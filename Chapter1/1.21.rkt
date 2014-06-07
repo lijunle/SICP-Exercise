5dd65bb54235b1e002795521d0551170ca9b3b98
Junle Li <lijunle@gmail.com>
2014-06-08 02:34:27 +0800

    Add exercise 1.21

---
+++ Chapter1/1.21.rkt
@@ -0,0 +1,23 @@
+#lang racket
+
+(define (smallest-division n)
+  (find-division n 2))
+
+(define (find-division n test-division)
+  (cond ((divides? test-division n) test-division)
+        ((> (square test-division) n) n)
+        (else (find-division n (+ 1 test-division)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (square n)
+  (* n n))
+
+; exercise
+
+(smallest-division 199)
+
+(smallest-division 1999)
+
+(smallest-division 19999)
